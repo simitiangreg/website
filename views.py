b1715a5a93a39c2145983ac23bd68bc716cfcef3
simitiangreg <simitiangreg@berkeley.edu>
2023-06-08 13:25:30 -0700

    changes

--- views.py
+++ views.py
@@ -7,14 +7,10 @@ views = Blueprint(__name__, "views")  # Creating a blueprint named 'views'
 def home():
     return render_template("home.html")
 
-@views.route('/about')
-def about():
-    return render_template('about.html')
-
 @views.route('/cv')
 def cv():
     return render_template('cv.html')
 
-@views.route('/blog')
+@views.route('/thoughts')
 def blog():
     return render_template('blog.html')
